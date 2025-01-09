fbbde56836903b46f5478a2d22943a20ed0d2b2d
Heitor Martini <martiniheitor@gmail.com>
2025-01-09 20:15:17 -0300

    errado

--- src/test/java/com/example/demo/ApplicationTests.java
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -4,10 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class ApplicationTests {
 
 	@Test
